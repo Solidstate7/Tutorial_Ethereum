36440b5b3e0807118e9859f5d22122cf1d0826d4
Eunjae Lee <vanitasium@gmail.com>
2024-02-05 17:15:57 +0900

    test code, react project init

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -1,24 +1,21 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
- 
-const { abi, evm } = require('./compile');
- 
-const provider = new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA
-);
- 
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA);
 const web3 = new Web3(provider);
- 
+
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
- 
+
   console.log('Attempting to deploy from account', accounts[0]);
- 
-  const result = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
- 
-  console.log('Contract deployed to', result.options.address);
+
+  console.log(interface);
+  console.log("Contract deployed to", result.options.address);
   provider.engine.stop();
 };
- 
 deploy();
